a4f1d24f9b3f9f3ca767db0f7475c4d67fd1873c
Brown-Wangeci <brownkamau630@gmail.com>
2024-10-14 18:38:03 +0300

    Favorites-card

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import Button from './components/button/button';
 import GeneratedImage from './components/generated-image/generatedImage';
 import { useState } from "react";
 import Loader from './components/loader/loader';
+import { FaCloudDownloadAlt } from "react-icons/fa";
+import { FaRegFaceSadCry } from "react-icons/fa6";
+
+
 
 function App() {
     const API_URL = 'https://cataas.com/cat';
@@ -37,13 +41,47 @@ function App() {
         }
     }
 
+    const downloadImage = async () => {
+        try {
+          const response = await fetch(catImage);
+          const blob = await response.blob();
+          const blobUrl = URL.createObjectURL(blob);
+    
+          // Create a temporary anchor element and trigger the download
+          const link = document.createElement('a');
+          link.href = blobUrl;
+          link.download = 'randomcat.png';
+          document.body.appendChild(link);
+          link.click();
+          document.body.removeChild(link);
+    
+          // Revoke the object URL to free up memory
+          URL.revokeObjectURL(blobUrl);
+        } catch (error) {
+          console.error('Failed to download the image:', error);
+        }
+      };
+
   return (
     <div className={styles.app}>
         <div className={styles.imgCard}>
-          {!loading  && <GeneratedImage catImage={catImage} key={imageCount} />}
-          {loading  && <Loader/>}
-          {error && <pre style={{color: "white"}}>{error}</pre>}
-          <Button fetchCatImage={fetchCatImage} />
+            {!loading  && <GeneratedImage catImage={catImage} key={imageCount} />}
+            {loading  && <Loader/>}
+            {error && <pre style={{color: "white"}}>{error}</pre>}
+            <Button fetchCatImage={fetchCatImage} />
+            {
+                catImage && (
+                    <div>
+                        <FaCloudDownloadAlt
+                        color  = 'pink' 
+                        size = '2em'
+                        onClick = {downloadImage}
+                        />
+                        <pre style={{color: 'white'}}>Disclaimer: Does not download the specific picture <FaRegFaceSadCry color  = 'pink' />
+                        </pre>
+                    </div>
+                )
+            }
         </div>
     </div>
   );
